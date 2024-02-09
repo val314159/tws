be03ea12d2df1d841d4bf106687d52b3e1ff6358
@val314159 <jmward@gmail.com>
2024-02-09 15:37:26 -0600

    update

--- Makefile
+++ Makefile
@@ -1,21 +1,21 @@
 call:: clean tree all
-all:: build serve
-build:: node_modules
-	tailwind -mo docs/site.css
-	mkdir -p docs/x
-	python filter.py <docs/index.html >docs/x/index.html
-	wc docs/index.html docs/x/index.html
+all:: build # serve
+build:: node_modules docs/site.css dist
+docs/site.css::;tailwind -mo docs/site.css
 node_modules: package.json ; bun i
 serve::;python -mhttp.server 80
 clean:: clean tree
 realclean:: clean
-	rm -fr auto-save-list
-	rm -fr __pycache__ node_modules
-	rm -fr yarn.lock bun.lockb package-lock.json
+	rm -fr auto-save-list dist docs/dist __pycache__
+	rm -fr yarn.lock bun.lockb package-lock.json node_modules
 	find . -name '\#*\#' -o -name '.\#*' | xargs rm -fr
 clean::
 	rm -fr docs/site.css docs/x
 	find . -name '*~' -o -name '.*~' | xargs rm -fr
 T=-I .git -I node_modules -I __pycache__
 tree:;	tree $T -a
-
+dist:
+	cd docs ; rm   -fr x
+	cd docs ; mkdir -p x
+	cd docs ; ../filter.py index.html x/index.html
+	cd docs ; wc index.html x/index.html
--- filter.py
+++ filter.py
@@ -1,10 +1,90 @@
-import os, sys
+#!/usr/bin/env python
+"""
+Usage:
+  PROG <input> <output>
+  PROG (-h | --help | --version)
 
-for line in sys.stdin:
-    if 'site.css' in line:
-        print(f"""<style>{
-            open('docs/site.css').read()
-        }</style>""")
-    else:
-        print(line, end='')
-    pass
+Options:
+  -h, --help  Show this screen and exit.
+  <input>     input file  [- means stdin]
+  <output>    output file [- means stdout]
+"""
+
+import os, sys, base64, docopt
+
+__version__ = "1.0.1"
+
+doc = __doc__.replace('PROG', sys.argv[0])
+
+args = docopt.docopt(doc, version=__version__)
+
+def contains(line, *stuff):
+    for thing in stuff:
+        if thing not in line:
+            return False
+        pass
+    return True
+
+
+def read_file(fname):
+    return open(fname).read()
+
+
+def read_file64(fname):
+    return base64.b64encode(
+        open(fname, 'rb').read()).decode()
+
+
+css_file = 'site.css'
+
+
+favicon_names = [ 'favicon-16x16.png' ,
+                  'favicon-32x32.png' ,
+                 ]
+
+
+def process_line(line):
+
+    if contains(line, '<link', 'stylesheet', css_file):
+        
+        return '<style>' + read_file(css_file) + '</style>\n'
+
+    for fname in favicon_names:
+        
+        if contains(line, '<link', 'icon', fname):
+            
+            data = "data:image/x-icon;," + read_file64(fname)
+            
+            return line.replace(fname, data)
+
+        pass
+    
+    return line
+
+
+def get_input():
+    
+    x = args['<input>']
+    
+    if x == '-':
+        return sys.stdin
+    
+    return open(x)
+
+
+def get_output():
+    
+    x = args['<output>']
+    
+    if x == '-':
+        return sys.stdout
+    
+    return open(x,'w')
+
+
+output = get_output()
+
+for line in get_input():
+    
+    print(process_line(line), end='', file=output)
+

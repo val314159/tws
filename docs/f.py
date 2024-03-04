6cec904b099dfa24629819b54d9d456f62c72889
@val314159 <jmward@gmail.com>
2024-03-04 15:56:08 -0600

    update

--- docs/f.py
+++ docs/f.py
@@ -2,24 +2,39 @@
 
 import re, os, sys
 
-for line in sys.stdin:
+infile = open(sys.argv[1]) if os.isatty(0) else sys.stdin
+
+def extract(ws, t, attrs, body=''):
+    if body: body = ' ' + body
+    if m2 := re.match('(.*?)class="(.*?)"(.*)$', attrs):
+        cl = m2.group(2).replace(' ', '.')
+        cl = cl.replace(':','__COLON__')
+        cl = cl.replace('/','__SLASH__')
+        cl = cl.strip()
+        if x := (m2.group(1) + m2.group(3)).strip():
+            print(f"{ws}{t}.{cl}({x}){body}")
+        else:
+            print(f"{ws}{t}.{cl}{body}")
+    elif attrs:
+        print(f"{ws}{t}({attrs}){body}")
+    else:
+        print(f"{ws}{t}{body}")
+        pass
+    pass
+
+
+for line in infile:
 
     line = line.replace('\t',' '*8)
     
-    #print("L", line, end='')
     if   m := re.match(r'(\s*)$', line):
-
-        #print()
-        #print('Q', line, end='')
         pass
     
     elif m := re.match(r'(\s*)<!--', line):
-        #print()
         pass
     
     elif m := re.match(r'(\s*)<!', line):
         print("!!!5")
-        #print("doctype html")
 
     elif m := re.match(r'(\s*)<html (.*?)>', line):
         print(f"html({m.group(2)})")
@@ -27,7 +42,7 @@ for line in sys.stdin:
     elif m := re.match(r'(\s*)<hr\s*/?>', line):
         print(f"{m.group(1)}hr")
 
-    elif m := re.match(r'(\s*)<(meta|x?link) (.*?)/?>', line):
+    elif m := re.match(r'(\s*)<(meta|x?link) (.*?)\s*/?>', line):
         print(f"{m.group(1)}{m.group(2)}({m.group(3)})")
 
     elif m := re.match(r'(\s*)<(x?script) (.*?)></x?script>', line):
@@ -46,89 +61,89 @@ for line in sys.stdin:
         print(f"{m.group(1)}nav")
 
     elif m := re.match(r'(\s*)<footer (.*?)>$', line):
-        print(f"{m.group(1)}footer({m.group(2)})")
+        extract(m.group(1), 'footer', m.group(2))
 
     elif m := re.match(r'(\s*)<div (.*?)>(.*?)</div>', line):
-        print(f"{m.group(1)}div({m.group(2)}) {m.group(3)}")
+        extract(m.group(1), 'div', m.group(2), m.group(3))
 
     elif m := re.match(r'(\s*)<div (.*?)>$', line):
-        print(f"{m.group(1)}div({m.group(2)})")
+        extract(m.group(1), 'div', m.group(2))
 
     elif m := re.match(r'(\s*)<div (.*?)>(.*?)', line):
         nope
         print(f"{m.group(1)}div({m.group(2)}) {m.group(3)}")
 
     elif m := re.match(r'(\s*)<svg (.*?)>(.*?)</svg>', line):
-        print(f"{m.group(1)}svg({m.group(2)}) {m.group(3)}")
+        extract(m.group(1), 'svg', m.group(2), m.group(3))
 
     elif m := re.match(r'(\s*)<a (.*?)>(.*?)</a>', line):
-        print(f"{m.group(1)}a({m.group(2)}) {m.group(3)}")
+        extract(m.group(1), 'a', m.group(2), m.group(3))
 
     elif m := re.match(r'(\s*)<time (.*?)>(.*?)</time>', line):
-        print(f"{m.group(1)}time({m.group(2)}) {m.group(3)}")
+        extract(m.group(1), 'time', m.group(2), m.group(3))
 
     elif m := re.match(r'(\s*)<span (.*?)>(.*?)</span>', line):
-        print(f"{m.group(1)}span({m.group(2)}) {m.group(3)}")
+        extract(m.group(1), 'span', m.group(2), m.group(3))
 
     elif m := re.match(r'(\s*)<span (.*?)>$', line):
-        print(f"{m.group(1)}span({m.group(2)})")
+        extract(m.group(1), 'span', m.group(2))
 
     elif m := re.match(r'(\s*)<figure>(.*?)</figure>', line):
-        print(f"{m.group(1)}figure {m.group(2)}")
+        extract(m.group(1), 'figure', m.group(2))
 
     elif m := re.match(r'(\s*)<h1 (.*?)>(.*?)</h1>', line):
-        print(f"{m.group(1)}h1({m.group(2)}) {m.group(3)}")
+        extract(m.group(1), 'h1', m.group(2), m.group(3))
 
     elif m := re.match(r'(\s*)<h2 (.*?)>(.*?)</h2>', line):
-        print(f"{m.group(1)}h2({m.group(2)}) {m.group(3)}")
+        extract(m.group(1), 'h2', m.group(2), m.group(3))
 
     elif m := re.match(r'(\s*)<h3 (.*?)>(.*?)</h3>', line):
-        print(f"{m.group(1)}h3({m.group(2)}) {m.group(3)}")
+        extract(m.group(1), 'h3', m.group(2), m.group(3))
 
     elif m := re.match(r'(\s*)<h4 (.*?)>(.*?)</h4>', line):
-        print(f"{m.group(1)}h4({m.group(2)}) {m.group(3)}")
+        extract(m.group(1), 'h4', m.group(2), m.group(3))
 
     elif m := re.match(r'(\s*)<h5 (.*?)>(.*?)</h5>', line):
-        print(f"{m.group(1)}h5({m.group(2)}) {m.group(3)}")
+        extract(m.group(1), 'h5', m.group(2), m.group(3))
 
     elif m := re.match(r'(\s*)<h6 (.*?)>(.*?)</h6>', line):
-        print(f"{m.group(1)}h6({m.group(2)}) {m.group(3)}")
+        extract(m.group(1), 'h6', m.group(2), m.group(3))
 
     elif m := re.match(r'(\s*)<h1>(.*?)</h1>', line):
-        print(f"{m.group(1)}h1 {m.group(2)}")
+        extract(m.group(1), 'h1', '', m.group(2))
 
     elif m := re.match(r'(\s*)<h2 (.*?)>$', line):
-        print(f"{m.group(1)}h2({m.group(2)})")
+        extract(m.group(1), 'h2', m.group(2))
         
     elif m := re.match(r'(\s*)<ul (.*?)>$', line):
-        print(f"{m.group(1)}ul({m.group(2)})")
+        extract(m.group(1), 'ul', m.group(2))
         
     elif m := re.match(r'(\s*)<p>(.*?)</p>', line):
-        print(f"{m.group(1)}p {m.group(2)}")
+        extract(m.group(1), 'p', '', m.group(2))
 
     elif m := re.match(r'(\s*)<p (.*?)>$', line):
-        print(f"{m.group(1)}p({m.group(2)})")
+        extract(m.group(1), 'p', m.group(2))
 
     elif m := re.match(r'(\s*)<button (.*?)>(.*?)</button>', line):
-        print(f"{m.group(1)}button({m.group(2)}) {m.group(3)}")
+        extract(m.group(1), 'button', m.group(2), m.group(3))
         
     elif m := re.match(r'(\s*)<button (.*?)>$', line):
-        print(f"{m.group(1)}button({m.group(2)})")
+        extract(m.group(1), 'button', m.group(2))
 
-    elif m := re.match(r'(\s*)<img (.*?)>$', line):
-        print(f"{m.group(1)}img({m.group(2)})")
+    elif m := re.match(r'(\s*)<img (.*?)/?>$', line):
+        extract(m.group(1), 'img', m.group(2))
 
     elif m := re.match(r'(\s*)<label (.*?)>$', line):
-        print(f"{m.group(1)}label({m.group(2)})")
+        extract(m.group(1), 'label', m.group(2))
 
-    elif m := re.match(r'(\s*)<input (.*?)>$', line):
-        print(f"{m.group(1)}input({m.group(2)})")
+    elif m := re.match(r'(\s*)<input (.*?)/?>$', line):
+        extract(m.group(1), 'input', m.group(2))
 
     elif m := re.match(r'(\s*)<section (.*?)>$', line):
-        print(f"{m.group(1)}section({m.group(2)})")
+        extract(m.group(1), 'section', m.group(2))
 
     elif m := re.match(r'(\s*)<article (.*?)>$', line):
-        print(f"{m.group(1)}article({m.group(2)})")
+        extract(m.group(1), 'article', m.group(2))
 
     elif m := re.match(r'(\s*)<li>(.*?)</li>', line):
         print(f"{m.group(1)}li {m.group(2)}")
@@ -140,7 +155,6 @@ for line in sys.stdin:
         print(f"{m.group(1)}p")
 
     elif m := re.match(r'(\s*)</', line):
-        #print()
         pass
 
     elif m := re.match(r'(\s*)<', line):
